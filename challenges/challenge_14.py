39cd512576c10bf7bba08c4ece81d06a14c6d40e
ysaunier <ysaunier@poka.io>
2020-02-11 19:49:16 -0500

    challenge 14 completed

--- challenges/challenge_14.py
+++ challenges/challenge_14.py
@@ -4,13 +4,17 @@ import settings
 from core.client import RingClient
 
 
+def decode_binary_string(text) -> str:
+    return ''.join(chr(int(text[i * 8:i * 8 + 8], 2)) for i in range(len(text) // 8))
+
+
 def execute():
-    client = RingClient(challenge=13, cookie=settings.SESSION_ID)
+    client = RingClient(challenge=14, cookie=settings.SESSION_ID)
     page = client.get_challenge()
     message = page.find('div', attrs={'class': 'message'})
     text = message.contents[2].strip()
 
-    response = hashlib.sha512(text.encode()).hexdigest()
+    response = hashlib.sha512(decode_binary_string(text=text).encode()).hexdigest()
 
     print(client.send_answer(response=response))
 
